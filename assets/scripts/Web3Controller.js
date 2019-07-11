80cc540a7f5266f8106a230e57278091e202fda7
Frog Dev <frog.dev.studio@gmail.com>
2019-07-11 17:55:12 +0700

    feat: set default value, update smart contract

--- assets/scripts/Web3Controller.js
+++ assets/scripts/Web3Controller.js
@@ -1,10 +1,11 @@
-const Web3 = require('web3.min');
+const Web3 = require("web3.min");
 
-const TOMOLUCKYBOX_CONTRACT_ADRESS_TESTNET = '0x48dBECEce748B2E47A577295176E390928435D22';
+const TOMOLUCKYBOX_CONTRACT_ADRESS_TESTNET =
+  "0x6f6591b7F61EB82BCeF137792237263a428ef83d";
 
-const PRICE_PER_OPEN = '1'; //1 TOMO
+const PRICE_PER_OPEN = "1"; //1 TOMO
 
-const GAS_PRICE_DEFAULT = '20000000000'; //20Gweis
+const GAS_PRICE_DEFAULT = "20000000000"; //20Gweis
 
 const isWeb3Enabled = () => !!window.web3;
 
@@ -14,12 +15,12 @@ const Web3Controller = cc.Class({
   properties: {
     Web3: null,
     Web3Provider: null,
-    Web3ProviderName: 'metamask',
+    Web3ProviderName: "metamask",
 
     Contract: null,
     ContractABI: cc.JsonAsset,
 
-    CurrentAccount: '',
+    CurrentAccount: ""
   },
 
   statics: {
@@ -32,9 +33,7 @@ const Web3Controller = cc.Class({
     Web3Controller.instance.initWeb3();
   },
 
-  start() {
-
-  },
+  start() {},
 
   // update (dt) {},
 
@@ -45,96 +44,118 @@ const Web3Controller = cc.Class({
 
       //Request account access for modern dapp browsers
       if (window.ethereum) {
-        this.Web3ProviderName = 'metamask';
+        this.Web3ProviderName = "metamask";
         this.Web3Provider = window.ethereum;
         this.Web3.setProvider(this.Web3Provider);
-        window.ethereum.enable().then((accounts) => {
-          Web3Controller.instance.initAccount();
-          Web3Controller.instance.initContract();
-        })
-          .catch((error) => {
+        window.ethereum
+          .enable()
+          .then(accounts => {
+            Web3Controller.instance.initAccount();
+            Web3Controller.instance.initContract();
+          })
+          .catch(error => {
             cc.error(error);
           });
       }
       //Request account access for legacy dapp browsers
       else if (window.web3) {
-        this.Web3ProviderName = 'tomowallet';
+        this.Web3ProviderName = "tomowallet";
         this.Web3Provider = window.web3.currentProvider;
         this.Web3.setProvider(this.Web3Provider);
 
         Web3Controller.instance.initAccount();
         Web3Controller.instance.initContract();
       }
-    }
-    else {
-      cc.error('You must enable and login into your TomoWallet or MetaMask accounts!');
+    } else {
+      cc.error(
+        "You must enable and login into your TomoWallet or MetaMask accounts!"
+      );
     }
   },
 
   initContract() {
-    this.Contract = new this.Web3.eth.Contract(this.ContractABI.json.abi, TOMOLUCKYBOX_CONTRACT_ADRESS_TESTNET);
+    this.Contract = new this.Web3.eth.Contract(
+      this.ContractABI.json.abi,
+      TOMOLUCKYBOX_CONTRACT_ADRESS_TESTNET
+    );
   },
 
   //Init current account address
   initAccount() {
-    this.Web3.eth.getAccounts().then((accounts) => {
+    this.Web3.eth.getAccounts().then(accounts => {
       if (accounts.length > 0) {
         Web3Controller.instance.CurrentAccount = accounts[0].toLowerCase();
-        HUDController.instance.updateAccountText(Web3Controller.instance.CurrentAccount);
+        HUDController.instance.updateAccountText(
+          Web3Controller.instance.CurrentAccount
+        );
         Web3Controller.instance.initBalance();
-      }
-      else
-        cc.error('You must enable and login into your TomoWallet or MetaMask accounts!');
+      } else
+        cc.error(
+          "You must enable and login into your TomoWallet or MetaMask accounts!"
+        );
     });
   },
 
   //Init current account balance
   initBalance() {
-    this.Web3.eth.getBalance(Web3Controller.instance.CurrentAccount, (err, balance) => {
-      if (err) {
-        cc.err(err);
-        return;
-      }
+    this.Web3.eth.getBalance(
+      Web3Controller.instance.CurrentAccount,
+      (err, balance) => {
+        if (err) {
+          cc.err(err);
+          return;
+        }
 
-      HUDController.instance.updateBalanceText(Web3Controller.instance.getTOMOFromWei(balance));
-    });
+        HUDController.instance.updateBalanceText(
+          Web3Controller.instance.getTOMOFromWei(balance)
+        );
+      }
+    );
   },
 
   sendRequestOpenLuckyBox() {
-    if(GameController.instance.selectedLuckyBoxId != 0) {
-      this.Contract.methods.randomLuckyBox(GameController.instance.selectedLuckyBoxId).send({
+    if (GameController.instance.selectedLuckyBoxId != 0) {
+      this.Contract.methods
+        .randomLuckyBox(GameController.instance.selectedLuckyBoxId)
+        .send({
           from: Web3Controller.instance.CurrentAccount,
-          value: Web3Controller.instance.Web3.utils.toWei(PRICE_PER_OPEN, 'ether'),
+          value: Web3Controller.instance.Web3.utils.toWei(
+            PRICE_PER_OPEN,
+            "ether"
+          ),
           gas: 250000,
           gasPrice: GAS_PRICE_DEFAULT
-      })
-      .on("transactionHash", (hash) => {
-         cc.log('transactionHash: ', hash);
-      })
-      .on("receipt", (receipt) => {
-        cc.log('receipt: ', receipt);
-        let returnValues = receipt.events.LogRandomLuckyBoxSuccessed.returnValues;
-        let result = parseInt(returnValues[1]);
-        Web3Controller.instance.initBalance();
-        GameController.instance.unselectAllLuckyBox();
-        GameController.instance.showResultOpenLuckyBox(result, 3);
-     })
-      .on("error", (error) => {
+        })
+        .on("transactionHash", hash => {
+          cc.log("transactionHash: ", hash);
+        })
+        .on("receipt", receipt => {
+          cc.log("receipt: ", receipt);
+          let returnValues =
+            receipt.events.LogRandomLuckyBoxSuccessed.returnValues;
+          let result = parseInt(returnValues[1]);
+          let amountPrize = Web3Controller.instance.getTOMOFromWei(
+            returnValues[3]
+          );
+          Web3Controller.instance.initBalance();
+          GameController.instance.unselectAllLuckyBox();
+          GameController.instance.showResultOpenLuckyBox(result, amountPrize);
+        })
+        .on("error", error => {
           cc.error("sendRequestOpenLuckyBox: ", error);
-      });
+        });
+    } else {
+      cc.error("sendRequestOpenLuckyBox failed!");
     }
-    else {
-        cc.error("sendRequestOpenLuckyBox failed!");
-    }
-},
+  },
 
   //Convert from Wei to TOMO
   getTOMOFromWei(value) {
-    return parseInt(Web3Controller.instance.Web3.utils.fromWei(value, 'ether'))
+    return parseInt(Web3Controller.instance.Web3.utils.fromWei(value, "ether"));
   }
 });
 
 module.exports = Web3Controller;
 
-const GameController = require('GameController');
-const HUDController = require('HUDController');
+const GameController = require("GameController");
+const HUDController = require("HUDController");
